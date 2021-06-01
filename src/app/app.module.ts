59ae53fb78886f02ac08613ac984c660456e3ab5
Minhaz <minhaz725@gmail.com>
2021-06-01 17:10:06 +0600

    signup half done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { HomeComponent } from './home/home.component';
 import {RouterModule} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
 import { SignupComponent } from './signup/signup.component';
+import { AdminProductsComponent } from './admin-products/admin-products.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SignupComponent
+    SignupComponent,
+    AdminProductsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent },
+      { path: '', component: WelcomeComponent },
+      { path: 'home', component: HomeComponent },
       { path: 'signup', component: SignupComponent}
     ]),
     HttpClientModule
