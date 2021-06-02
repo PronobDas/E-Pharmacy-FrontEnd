2900bc4bf700cb39acaacec93977cdd442f8cb73
Minhaz <minhaz725@gmail.com>
2021-06-02 07:02:28 +0600

    login,home,navbar done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import {HttpClientModule} from "@angular/common/http";
 import { SignupComponent } from './signup/signup.component';
 import { AdminProductsComponent } from './admin-products/admin-products.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
@@ -15,14 +18,20 @@ import { WelcomeComponent } from './welcome/welcome.component';
     HomeComponent,
     SignupComponent,
     AdminProductsComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    NavbarComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     RouterModule.forRoot([
-      { path: '', component: WelcomeComponent },
+      { path: '', component: HomeComponent },
       { path: 'home', component: HomeComponent },
-      { path: 'signup', component: SignupComponent}
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'signup', component: SignupComponent},
+      { path: 'signin', component: SigninComponent},
+      { path: 'admin-products', component: AdminProductsComponent}
     ]),
     HttpClientModule
   ],
