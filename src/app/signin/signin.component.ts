da8640bb91e09ed54b0a45b4790fdcfe85884da2
Pronob <pronobdas2k16@gmail.com>
2021-06-27 23:17:56 +0600

    model User name edited

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User} from "../models/user";
+import { User} from "../models/User";
 import {AuthService} from "../services/auth.service";
 import {Router} from "@angular/router";
 
